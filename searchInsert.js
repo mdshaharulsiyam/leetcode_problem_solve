ed1dd9f1ebe755f8eac4c028f103c70e335fe17a
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-29 09:42:03 -0600

    plus one

---
+++ searchInsert.js
@@ -0,0 +1,16 @@
+var searchInsert = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return start;
+};
