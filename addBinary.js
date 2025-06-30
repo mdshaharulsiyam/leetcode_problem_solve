8b8921d1252f679444b2a5ab74bc2e791beb77ba
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-30 17:40:54 +0600

    add binary

---
+++ addBinary.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+function addBinary(a, b) {
+  let result = '';
+  let carry = 0;
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (i >= 0 || j >= 0 || carry) {
+    const bitA = i >= 0 ? parseInt(a[i]) : 0;
+    const bitB = j >= 0 ? parseInt(b[j]) : 0;
+
+    const sum = bitA + bitB + carry;
+    result = (sum % 2) + result; // Append the current bit to the result
+    carry = Math.floor(sum / 2); // Calculate the carry
+
+    i--;
+    j--;
+  }
+
+  return result;
+}
+
+
